57158fe613e5af447d7215e9b2db14df2e35054a
Valentina <vrudnitskaya@gmail.com>
2024-09-23 08:50:19 -0500

    fix session controller

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); // to load the .env file into the process.env object\
 const express = require("express");
 require("express-async-errors");
 
@@ -6,7 +7,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(require("body-parser").urlencoded({ extended: true }));
 
-require("dotenv").config(); // to load the .env file into the process.env object
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const url = process.env.MONGO_URI;
--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -6,29 +6,32 @@ const registerShow = (req, res) => {
 };
 
 const registerDo = async (req, res, next) => {
-  if (req.body.password != req.body.password1) {
-    req.flash("error", "The passwords entered do not match.");
-    return res.render("register", {  errors: flash("errors") });
-  }
   try {
+    if (req.body.password !== req.body.password1) {
+      req.flash("errors", "The passwords entered do not match.");
+      return res.render("register", { errors: req.flash("errors"), formData: req.body });
+    }
+    
     await User.create(req.body);
+    return res.redirect("/");
   } catch (e) {
     if (e.constructor.name === "ValidationError") {
       parseVErr(e, req);
+      req.flash("errors", "There were validation errors.");
     } else if (e.name === "MongoServerError" && e.code === 11000) {
-      req.flash("error", "That email address is already registered.");
+      req.flash("errors", "That email address is already registered.");
     } else {
       return next(e);
     }
-    return res.render("register", {  errors: flash("errors") });
+    return res.render("register", { errors: req.flash("errors"), formData: req.body });
   }
-  res.redirect("/");
 };
 
 const logoff = (req, res) => {
   req.session.destroy(function (err) {
     if (err) {
       console.log(err);
+      return res.redirect("/"); 
     }
     res.redirect("/");
   });
